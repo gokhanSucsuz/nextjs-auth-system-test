c0a42fa5cba26d11a3920d369a77501db50d6215
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-10-29 21:40:33 +0300

    ...

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,7 +2,7 @@
 import { SignInForm } from "@/components/shared/SignInForm";
 import React from "react";
 const SignIn = () => {
-	return <div className="flex min-h-[calc(100vh-76px)] bg-slate-300 dark:bg-slate-800 flex-col items-center justify-center  max-width-[1200px] gap-20 px-2">
+	return <div className="flex min-h-[calc(100vh-100px)] bg-slate-300 dark:bg-slate-800 flex-col items-center justify-center  max-width-[1200px] gap-20 px-2">
 			<SignInForm />
 		</div>;
 };
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { SignUpForm } from "@/components/shared/SignUpForm";
 const SignIn = () => {
-	return <div className="flex min-h-[calc(100vh-76px)] bg-slate-300 dark:bg-slate-800 flex-col items-center justify-center  max-width-[1200px] gap-20 px-2">
+	return <div className="flex min-h-[calc(100vh-100px)] bg-slate-300 dark:bg-slate-800 flex-col items-center justify-center  max-width-[1200px] gap-20 px-2">
 			<SignUpForm />
 		</div>;
 };

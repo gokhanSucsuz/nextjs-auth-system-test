486741698558cf739b5f7e90515268f8ee2e30bb
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-10-29 21:28:39 +0300

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
         >
-          <div className="flex flex-col min-h-screen">
+          <div className="flex flex-col min-h-[calc(100vh-80px)]">
             <Navbar user={user && true} />
           <div className="w-full flex-grow">
             {children}
